2bb4621e23793dc3cade86b1ae118efe589336c2
godmod <godmod@eqdkp-plus.eu>
2014-05-02 09:22:28 +0000

    #745 MMO News wrong game name for Everquest 2 Created Tag 0.1.2

---
+++ language/german.php
@@ -0,0 +1,30 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2008
+ * Date:		$Date: 2011-08-08 17:25:17 +0200 (Mo, 08. Aug 2011) $
+ * -----------------------------------------------------------------------
+ * @author		$Author: wallenium $
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev: 10931 $
+ * 
+ * $Id: german.php 10931 2011-08-08 15:25:17Z wallenium $
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+$lang = array(
+	'mmo_news'					=> 'MMO-News',
+	'mmo_news_name'				=> 'MMO-News',
+	'mmo_news_desc'				=> 'Zeigt dir spannende Neuigkeiten rund um dein Spiel an',
+	'pm_mmo_news_count'			=> 'Anzahl der angezeigten News',
+	'pm_mmo_news_checkURL'		=> 'Überprüfe die Erreichbarkeit des Servers vor Abfragen der News',
+);
+?>
